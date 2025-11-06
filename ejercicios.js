ba88e639448fe4aa36ed5008870205e1065f977a
jorge emilio batun alcocer <jorge.batun@tecdesoftware.edu.mx>
2025-11-05 23:44:48 -0600

    ejercicio 5.3 resuelto

--- ejercicios.js
+++ ejercicios.js
@@ -312,7 +312,7 @@ function crearMatriz(filas, columnas) {
  */
 function sumaMatriz(matriz) {
   let suma = 0;
-  
+
   for (let i=0; i < matriz.length; i++){
     for (let j = 0; j < matriz[i].length; j++){
       suma += matriz [i][j];
@@ -332,7 +332,7 @@ function sumaMatriz(matriz) {
  */
 function obtenerFila(matriz, indiceFila) {
   // TODO: Retorna la fila indicada
-  return [];
+  return matriz[indiceFila];
 }
 
 /**
